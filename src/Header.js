7592cda11c52660ca6dfc7a9d7abd2a80496a449
Adeeladi1 <adirockx0013@gmail.com>
2021-08-30 02:21:17 +0500

    todo-prac

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import './index.css'
+const Header = () => {
+    return (
+        <header style={{color:"white" , textAlign:"center", fontFamily:"Times New Roman, Times, serif" }}>
+            <h1>To Do List</h1>
+        </header>
+    );
+};
+
+export default Header;
